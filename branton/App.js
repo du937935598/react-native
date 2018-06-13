87c77ac12de35741728a06145af276bbda7f0293
branton <937935598@qq.com>
2018-06-13 18:29:38 +0800

    aa

--- branton/App.js
+++ branton/App.js
@@ -1,107 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    View,
+    Text,
+    Button,
+    StyleSheet
 } from 'react-native';
-import TabBar from 'react-native-xtabbar';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class Main extends React.Component {
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        badge: ' '
-    };
-  }
-  _pressButton() {
-    // this.props.navigator.push({
-    //     title: 'Second',
-    //     component: Second
-    // })
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-          <TabBar
-          defaultPage={3}
-          style={styles.content}
-          onItemSelected={(index) => {console.log('current itemindex is ${index}');}}>
-              <TabBar.Item
-                  icon={require('./images/home.png')}
-                  selectedIcon={require('./images/home-active.png')}
-                  onPress={() => {
-                      this.setState({badge: ' ',});
-                  }}
-                  title='首页'>
-                  <View style={styles.text}>
-                    <Text style={{fontSize: 18}}>Home</Text>
-                  </View>
-              </TabBar.Item>
+    state = {
+        screenReaderEnabled: false,
+    }
 
-              <TabBar.Item
-                  icon={require('./images/invest.png')}
-                  selectedIcon={require('./images/invest-active.png')}
-                  onPress={() => {
-                      this.setState({badge: 5,});
-                  }}
-                  title='排行'>
-                  <View style={styles.text}>
-                    <Text style={{fontSize: 18}}>Rank</Text>
-                  </View>
-              </TabBar.Item>
+    onPressLearnMore(){
+        console.log('function')
+    }
 
-              <TabBar.Item
-                  icon={require('./images/find.png')}
-                  selectedIcon={require('./images/find-active.png')}
-                  badge={this.state.badge}
-                  onPress={() => {
-                      this.setState({badge: '',});
-                  }}
-                  title='消息'>
-                  <View style={styles.text}>
-                    <Text style={{fontSize: 18}}>Message</Text>
-                  </View>
-              </TabBar.Item>
-
-              <TabBar.Item
-                  icon={require('./images/account.png')}
-                  selectedIcon={require('./images/account-active.png')}
-                  title='我'>
-                    <View style={styles.text}>
-                      <TouchableOpacity onPress={this._pressButton.bind(this)}>
-                        <Text style={{fontSize: 18}}>Me</Text>
-                      </TouchableOpacity>
-                    </View>
-              </TabBar.Item>
-          </TabBar>
-      </View>
-    );
-  }
+    render() {
+      return(
+        <View style={styles.container}>
+            <Text>
+            The screen reader is {this.state.screenReaderEnabled ? 'enabled' : 'disabled'}.
+            </Text>
+            <Button onPress={this.onPressLearnMore} title="Learn More" color="#841584" accessibilityLabel="Learn more about this purple button"/>
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
+  defaultText: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 50,
+  },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  content: {
+      flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  }
 });
